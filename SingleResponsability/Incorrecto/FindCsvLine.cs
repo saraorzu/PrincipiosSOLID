e889d4bc69c92c7bd90bd174d08cec9d3719a615
Saraoz <genkidama.sara@gmail.com>
2023-10-19 13:22:12 +0200

    Modify "S" example closes#7

---
+++ SingleResponsability/Incorrecto/FindCsvLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSOLID.SingleResponsability.Incorrecto
+{
+    public class FindCsvLine
+    {
+        //metodo incorrecto porque tiene 2 responsabilidades: Leer archivo CSV y buscar linea  
+        public string FindCSVLine(string day)
+        {
+            string path = "stocks-ITX.csv";
+            char csvSeparator = ';';
+            StreamReader file = new StreamReader(path);
+
+            string foundLine = string.Empty;
+            string line;
+
+            using (file)
+            {
+
+                file.ReadLine();
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] arrayLine = line.Split(csvSeparator);
+                    string date = arrayLine[0];
+
+                    if (date == day)
+                    {
+                        foundLine = line;
+                    }
+                }
+
+            }
+
+            return foundLine;
+        }
+
+    }
+
+}
