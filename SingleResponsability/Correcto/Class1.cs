4d05299ab86cc479f7938a14bcd87eb3c1d67865
Saraoz <genkidama.sara@gmail.com>
2023-10-19 12:38:45 +0200

    Added Single Responsability Implementation Closes #1

---
+++ SingleResponsability/Correcto/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSOLID.SingleResponsability.Correcto
+{
+    public class Class1
+    {
+        //clase de prueba que si cumple funcionalidades
+
+    }
+}
